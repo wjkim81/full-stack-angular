2a869f8d0f5068c2a3acfed3f104b79be8e6e6e2
wjkim81 <onejean81@gmail.com>
2018-03-16 20:55:33 +0900

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component'
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module'
 
@@ -40,7 +41,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module'
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [ DishService ],
+  providers: [ DishService,
+    PromotionService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
