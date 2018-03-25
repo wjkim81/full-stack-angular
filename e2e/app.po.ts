6170cbe62b50588b70348f9aa5047f5e451f5bf8
wjkim81 <onejean81@gmail.com>
2018-03-25 20:26:05 +0900

    End-to-End Test

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,20 @@
 import { browser, element, by } from 'protractor';
 
 export class ConFusionPage {
-  navigateTo() {
-    return browser.get('/');
+  navigateTo(link: string) {
+    return browser.get(link);
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getParagraphText(selector: string) {
+    return element(by.css(selector)).getText();
   }
+
+  getElement(selector: string) {
+    return element(by.css(selector));
+  }
+
+  getAllElements(selector: string) {
+    return element.all(by.css(selector));
+  }
+  
 }
